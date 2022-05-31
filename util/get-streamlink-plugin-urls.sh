279c29bd1e9c810f8e6e58117b177a6aa75bcac2
bgh <git@bgh.io>
2022-05-31 18:05:28 +1000

    Fix selector query. Switch from wget to curl

--- util/get-streamlink-plugin-urls.sh
+++ util/get-streamlink-plugin-urls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readarray -t plugin_urls < <(wget --quiet --output-document=- https://streamlink.github.io/plugins.html | hxnormalize | hxclean | hxselect -c '#plugins section dl dd.field-odd:first-of-type ul li p' | grep '\S' | tr --delete " ")
+readarray -t plugin_urls < <(curl --silent https://streamlink.github.io/plugins.html | hxnormalize | hxclean | hxselect -c '#plugins section dl dd.field-even:nth-of-type(2) ul li p' | grep '\S' | tr --delete " ")
 readarray -t plugin_urls < <(printf "\"*://*.%s/*\"\n" "${plugin_urls[@]}")
 
 echo "$(IFS=,; echo "${plugin_urls[*]}" | sed --expression='s/,/, /g')"
