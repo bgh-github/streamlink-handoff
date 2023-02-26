b2ff5b87d24f998f80321cd92088d6c06ddb5aa0
bgh <git@bgh.io>
2023-02-26 15:41:21 +1100

    Final message parameter string passed to Streamlink shouldn't be quoted

--- aur/streamlink-handoff.sh
+++ aur/streamlink-handoff.sh
@@ -4,4 +4,4 @@ message_byte_length=$(od --address-radix=n --read-bytes=4 --format=dL | tr --del
 message=$(od --address-radix=n --read-bytes="${message_byte_length}" --format=x1 | xxd --plain --revert)
 message=$(echo "${message}" | sed --expression='s/^"\(.*\)"$/\1/')
 
-streamlink "${message}" > /dev/null 2>&1
+streamlink ${message} > /dev/null 2>&1
